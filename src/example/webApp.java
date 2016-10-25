bad39daa5eaa31b4bd24a9b0ca4501fda658db2f
mwynne777 <Michael@MacBook-Pro.local>
2016-10-24 20:45:14 -0400

    HTML_PAGES

--- src/example/webApp.java
+++ src/example/webApp.java
@@ -9,11 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 
 public class webApp extends HttpServlet{
 
-	@Override
+	/*@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		resp.getWriter().println("TESTING.... TOMCAT...");
 		//super.doGet(req, resp);
-	}
+	}*/
+	
+	public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+    throws ServletException, IOException
+    {
+		// Set response content type
+		response.setContentType("text/html");
+
+		// New location to be redirected
+		String site = new String("http://www.photofuntoos.com");
+
+		response.setStatus(response.SC_MOVED_TEMPORARILY);
+		response.setHeader("Location", site);    
+    }
 
 }
--- src/org/asastudio/form/javaForm.java
+++ src/org/asastudio/form/javaForm.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class javaForm extends HttpServlet {
 
-	@Override
+	/*@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		resp.getWriter().println("TESTING SUCCESS!!!");
@@ -17,6 +17,19 @@ public class javaForm extends HttpServlet {
 		String user_healthy = req.getParameter("userHealthy");
 		resp.getWriter().println(user_location);
 		resp.getWriter().println(user_healthy);
-	}
+	}*/
 	
+	
+	public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+    throws ServletException, IOException
+    {
+		// Set response content type
+		response.setContentType("text/html");
+
+		// New location to be redirected
+		String site = new String("http://localhost:8080/FoodFinder/results.html");
+		response.setStatus(response.SC_MOVED_TEMPORARILY);
+		response.setHeader("Location", site);    
+    }
 }
